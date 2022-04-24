e95b5abe7e1b9c7a95287ca9b95ba7aed174d54c
Amit <chaudhary.amit2058@gmail.com>
2022-04-24 19:16:50 +0545

    feat: text convert uppercase to lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter text here2");
-    const handleUpClick = () =>{
-        // console.log("Uppercase was clicked" + text);
-        let newText = text.toUpperCase();
-        setText(newText)
-    }
+  const [text, setText] = useState("");
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event) =>{
-        // console.log("OnChange");
-        setText(event.target.value)
-    }
+  const handleOnChange = (event) => {
+    // console.log("OnChange");
+    setText(event.target.value);
+  };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className='btn btn-primary' onClick={handleUpClick}>Convert to uppercase</button>
-    </div>
-  )
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words, and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
